8e048599ee3e10d8c0fcbb61a2228947382cf89a
binnlee <ninhlb.23ba14224@usth.edu.vn>
2026-09-18 20:02:46 +0700

    style: sync frontend directory with main branch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      // Chuyển hướng toàn bộ request API sang Backend Node.js
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+    allowedHosts: true,
+  },
 })
